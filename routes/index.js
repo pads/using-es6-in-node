f1185248481bcbe9968393874d0b0eeaa2461966
Ben Paddock <pads@thisispads.me.uk>
2015-10-24 18:23:38 +0100

    Set title to BrumES6 and link to users list

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function(request, response) {
-    response.render("index", { title: "Express" });
+    response.render("index", { title: "BrumES6" });
 });
 
 module.exports = router;
--- views/index.js
+++ views/index.js
@@ -2,5 +2,6 @@ module.exports = function template (params) {
     return `
         <h1>${params.title}</h1>
         <p>Welcome to ${params.title}</p>
+        <a href="/users">View Users</a>
     `;
 };
